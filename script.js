83259545f9be311339e6ce565c4a8b7c2c5892cc
B-pham <b.pham48@gmail.com>
2024-01-01 00:54:52 -0600

    Learning objects and function-variable notation

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 'use strict';
 
-const x = '23';
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
 
-const drink = x >= 18 ? 'wine':'water';
-console.log(drink);
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Bob'));
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends:['Michael', 'Peter', 'Steven']
+}
+
+console.log(jonas);
