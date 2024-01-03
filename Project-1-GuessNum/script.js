0f2f017fd2dfc76143405790acd1fa59045f87d3
B-pham <b.pham48@gmail.com>
2024-01-03 01:09:57 -0600

    added game logic, fixed UI

--- Project-1-GuessNum/script.js
+++ Project-1-GuessNum/script.js
@@ -1,2 +1,26 @@
 "use strict";
 
+/*console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);*/
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No Number!';
+    }else if(guess > 5){
+        document.querySelector('.message').textContent = 'Too High!';
+    }else if (guess < 5){
+        document.querySelector('.message').textContent = 'Too Low!';
+    }else{
+        document.querySelector('.message').textContent = 'Correct!';
+    }
+
+
+});
