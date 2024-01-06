0da3dc7d59ba7086d78776460a948baf2cd25c4e
B-pham <b.pham48@gmail.com>
2024-01-05 23:34:45 -0600

    added button functionality

--- Project-2-ModalWindow/script.js
+++ Project-2-ModalWindow/script.js
@@ -1,2 +1,30 @@
 'use strict'
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+function hideModal(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', function(){
+        console.log("button clicked");
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+}
+
+btnCloseModal.addEventListener('click', hideModal);
+overlay.addEventListener('click', hideModal);
+
+document.addEventListener('keydown', function(event){
+    if(event.key === 'Escape' && !modal.classList.contains('hidden')){
+        hideModal();
+    }
+});
+
+
