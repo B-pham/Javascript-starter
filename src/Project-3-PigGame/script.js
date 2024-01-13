0ac851b6a595e12a52a7df6cd19cac3c68a8d18c
B-pham <b.pham48@gmail.com>
2024-01-12 20:34:45 -0600

    added indicator on active player

--- src/Project-3-PigGame/script.js
+++ src/Project-3-PigGame/script.js
@@ -1,10 +1,10 @@
 "use strict"
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-const curr0 = document.getElementById('current--0');
-const curr1 = document.getElementById('current--1');
-
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -15,7 +15,10 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0,0];
 let currScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function(){
     
@@ -27,10 +30,14 @@ btnRoll.addEventListener('click', function(){
 
     if(dice !== 1){
         currScore += dice;
-        curr0.textContent = currScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currScore;
     }else{
         currScore = 0;
-        curr0.textContent = currScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currScore;
+
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 
 });
